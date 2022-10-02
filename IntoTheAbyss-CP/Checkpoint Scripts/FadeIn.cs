f91eb8e10ee4b6de29de35a1761ed92f6610ca3a
Wisni1ne <54714888+Wisni1ne@users.noreply.github.com>
2022-10-01 22:31:43 -0400

    Add files via upload

---
+++ IntoTheAbyss-CP/Checkpoint Scripts/FadeIn.cs
@@ -0,0 +1,33 @@
+﻿/*
+    ----------------------------------------------------------------------------------------------
+    FILE NAME: FadeIn.cs
+    AUTHOR: Nathan Wisniewski
+    LAST REVISION DATE: April 20, 2021
+    
+    DESCRIPTION: Play a fade-in animation once the scene is loaded.
+    ----------------------------------------------------------------------------------------------
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+
+    public Animator FadeInAni;
+
+    /*
+     *  FUNCTION NAME: Start
+     *  RETURNS: IEnumerator (Wait for time)
+     *  
+     *  DESCRIPTION: When starting, play the fade in animation.
+     */
+
+    IEnumerator Start()
+    {
+        FadeInAni.SetTrigger("Start");
+        yield return new WaitForSeconds(1.0f);      // Length of animation.
+        FadeInAni.SetTrigger("End");
+    }
+}
